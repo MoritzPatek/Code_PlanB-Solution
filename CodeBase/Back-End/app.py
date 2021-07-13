3612f38f27f55c0d859c7c5dcb595cf60c983793
Moritz Patek <MoritzPatek@MacBook-Air-von-Ilona.local>
2021-07-13 11:01:10 +0200

    backend routes

--- CodeBase/Back-End/app.py
+++ CodeBase/Back-End/app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask
 from flask import Flask, jsonify,request
 import mysql.connector
@@ -7,9 +8,29 @@ app = Flask(__name__)
 
 mycursor = mydb.cursor()
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/get_random_activity')
+def get_random_activity():
+    return "random activity"
+
+@app.route('/get_specific_activity')
+def get_specific_activity():
+    json_data = flask.request.json
+    personCount = json_data['personCount']
+    budget = json_data['budget']
+    dogFriendly = bool(json_data['dogFriendly'])
+    kidFriendly = bool(json_data['kidFriendly'])
+    kidPause = bool(json_data['kidPause'])
+
+    return 'specific_activity'
+
+@app.route('/add_activity')
+def add_activity():
+    return 'activity got added'
+
+@app.route('/delete_activity')
+def delete_activity():
+    return 'activity got deleted'
+
 
 
 if __name__ == '__main__':
